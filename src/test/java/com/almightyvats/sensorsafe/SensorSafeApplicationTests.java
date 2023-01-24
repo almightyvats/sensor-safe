76070f623381fa75a47e6b6adcbafbdfc0abbba7
Anurag Vats <ianuragvats@gmail.com>
2023-01-25 00:48:25 +0100

    fix: create spring-boot application
    Create initial commit for the sensor-safe spring boot application.

---
+++ src/test/java/com/almightyvats/sensorsafe/SensorSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almightyvats.sensorsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
